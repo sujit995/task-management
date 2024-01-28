0bc9ede15d95a662a5315ab20d4c8dca40326527
sujit995 <sujitmishra115@gmail.com>
2024-01-28 10:14:51 +0530

    fixed production issues

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -45,7 +45,7 @@ export default function Register() {
             name="email"
           />
           <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
+            We&#63;ll never share your email with anyone else.
           </div>
         </div>
         <div className="mb-3">
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -51,7 +51,7 @@ export default function Register() {
             name="email"
           />
           <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
+            We&#63;ll never share your email with anyone else.
           </div>
         </div>
         <div className="mb-3">
